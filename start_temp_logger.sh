edc605e257cddbeac1d465e4c56e0b96b5447251
Mathieu Good <mathieugood@MatGoodMac.local>
2023-10-04 00:14:26 +0200

    Initial commit

--- start_temp_logger.sh
+++ start_temp_logger.sh
@@ -2,7 +2,7 @@
 while true; do
 
     # Secret Key
-    key='b57d2452e5f1e580dccde5ca5cad922'
+    key='1b57d2452e5f1e580dccde5ca5cad922'
 
     # Token
     token='af38398c2ef42575b6d5a85b464a3eabc8066b0c660d2d7981f862be094e6ecf61069abf984c04f9f6d02ee6667d4976'
